6f3be0ad875fd0e4b7cf825c8b465d71709cbbf3
Eugene851001 <jackz1630@mail.ru>
2020-09-25 12:31:56 +0300

    Added second text table

--- main.cpp
+++ main.cpp
@@ -2,11 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <vector>
 
 #define PI 3.1415
 
+#define TABLE_DELIMITER '|'
+
+#define MODE_CIRCLE 0
+#define MODE_TABLE1 1
+#define MODE_TABLE2 2
+
+#define IDM_CIRCLE 0
+#define IDM_TABLE1 1
+#define IDM_TABLE2 2
+#define IDM_SAVE 3
+#define IDM_OPEN 4
+
+int currentMode = MODE_CIRCLE;
+
 void drawCircleText(HDC hdc, HWND hWnd, int x, int y);
 void drawTextTable(HDC hdc, HWND hWnd, int rows, int cols);
+void addMenu(HWND hWnd);
+void onCommand(HWND hWnd, WPARAM wParam);
+void WriteFile(const char* fileName);
+void loadTable(const char* fileName);
+void drawSecondTextTable(HDC hdc, HWND hWnd);
+int getColsAmount();
+char* showOpenTableDialog(HWND hWnd);
+
+typedef std::vector<char*> TTableRow;
+typedef std::vector<TTableRow> TTable;
+
+TTable textTable;
 
 char* strOutput;
 float radius = 50;
@@ -16,6 +43,175 @@ char strTemp[2];
 
 int cols = 5;
 int rows = 5;
+
+char* showOpenTableDialog(HWND hWnd)
+{
+	OPENFILENAME ofn;
+    char szFile[260];
+
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hWnd;
+	ofn.lpstrFile = szFile;
+	// Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+	// use the contents of szFile to initialize itself.
+	ofn.lpstrFile[0] = '\0';
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = "All\0*.*\0Text\0*.TXT\0";
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = NULL;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+	
+	
+	if(GetOpenFileName(&ofn) == TRUE)
+	{
+		loadTable(szFile);
+		return ofn.lpstrFile;
+	}
+	else
+	{
+		DWORD error = CommDlgExtendedError();
+		char buffer[256];
+		itoa(error, buffer, 16);
+		MessageBox(hWnd, buffer, "Error", MB_OK | MB_ICONERROR);
+		return NULL;
+	}
+}
+
+void loadTable(const char* fileName)
+{
+	FILE *f;
+	f = fopen(fileName, "r");
+	if(!f)
+		return;
+	fseek(f, 0, SEEK_END);
+	int length = ftell(f);
+	char* buffer = (char*)malloc(length);
+	fseek(f, 0, SEEK_SET);
+	fread(buffer, 1, length, f);
+	fclose(f);
+	buffer[length] = 0;
+	
+	TTableRow* row = new TTableRow();
+	char c;
+	std::vector<char>* strTemp = new std::vector<char>();
+	for(int  i = 0; i < length; i++)
+	{
+		c = buffer[i];
+		if(c == TABLE_DELIMITER)
+		{
+			strTemp->push_back(0);
+			row->push_back((char*)&strTemp->at(0));
+			strTemp = new std::vector<char>();
+		}
+		else if(c == 10)
+		{
+			strTemp->push_back(0);
+			row->push_back((char*)&strTemp->at(0));
+			textTable.push_back(*row);
+			row = new TTableRow();
+			strTemp = new std::vector<char>();
+		}
+		else
+		{
+			strTemp->push_back(c);
+		}
+	}
+	strTemp->push_back(0);
+	row->push_back(&strTemp->at(0));
+	textTable.push_back(*row);
+}
+
+int getColsAmount()
+{
+	int colsAmount = 0;
+	TTable::iterator tableIterator = textTable.begin();
+	while(tableIterator != textTable.end())
+	{
+		if(tableIterator->size() > colsAmount)
+		{
+			colsAmount = tableIterator->size();
+		}
+		tableIterator++;
+	}
+	return colsAmount;
+}
+
+void drawSecondTextTable(HDC hdc, HWND hWnd)
+{
+	int cols = getColsAmount();
+	int rows = textTable.size();
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	int width = rect.right / cols;
+	int height = rect.bottom / rows;
+	for(int row = 0; row < rows; row++)
+	{
+		for(int col = 0; col < cols; col++)
+		{
+			RECT cellRect;
+			cellRect.left = col * width;
+			cellRect.right = cellRect.left + width;
+			cellRect.top = row * height;
+			cellRect.bottom = cellRect.top + height;
+			Rectangle(hdc, cellRect.left, cellRect.top, cellRect.right, cellRect.bottom);
+			cellRect.top += height / 2;
+			if(col <= textTable[row].size())
+				DrawTextA(hdc, textTable[row][col], -1, &cellRect, DT_CENTER | DT_WORDBREAK);
+		}
+	}
+}
+
+void addMenu(HWND hWnd)
+{
+	HMENU hMenu = CreateMenu();
+	HMENU hModeSubmenu = CreatePopupMenu();
+	AppendMenuA(hMenu, MF_POPUP, (UINT_PTR)hModeSubmenu, "ChangeMode");
+	AppendMenuA(hModeSubmenu, MF_STRING, IDM_CIRCLE, "Circle");
+	AppendMenuA(hModeSubmenu, MF_STRING, IDM_TABLE1, "Table1");
+	AppendMenuA(hModeSubmenu, MF_STRING, IDM_TABLE2, "Table2");
+	AppendMenuA(hMenu, MF_STRING, IDM_SAVE, "Save");
+	AppendMenuA(hMenu, MF_STRING, IDM_OPEN, "Open");
+	SetMenu(hWnd, hMenu);
+}
+
+void onCommand(HWND hWnd, WPARAM wParam)
+{
+	switch (wParam)
+	{
+		case IDM_CIRCLE:
+			currentMode = MODE_CIRCLE;
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case IDM_TABLE1:
+			currentMode = MODE_TABLE1;
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case IDM_TABLE2:
+			currentMode = MODE_TABLE2;
+			loadTable("table.txt");
+		//	MessageBox(hWnd, "Table2 not implemented yet", "Sorry", MB_OK | MB_ICONINFORMATION);
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case IDM_SAVE:
+			WriteFile("Output.txt");
+			MessageBox(hWnd, "Saved", "OK", MB_OK | MB_ICONINFORMATION);
+			break;
+		case IDM_OPEN:
+		{
+			char* fileName;
+			fileName = showOpenTableDialog(hWnd);
+			if(fileName)
+				MessageBox(hWnd, fileName, "OK", MB_OK | MB_ICONINFORMATION);
+			else
+				MessageBox(hWnd, "Something wrong", "Not ok", MB_OK | MB_ICONERROR);
+			break;
+		}
+		
+	}
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -33,25 +229,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			RECT rect;
 			GetClientRect(hWnd, &rect);
 			
-			drawTextTable(ps.hdc, hWnd, rows, cols);
-						
-			LOGFONT lf;
-			lf.lfCharSet = DEFAULT_CHARSET;
-			lf.lfPitchAndFamily = FIXED_PITCH;
-			strcpy(lf.lfFaceName, "Courier New");
-			lf.lfHeight = 20;
-			lf.lfWidth = 10;
-			lf.lfWeight = 400;
-			lf.lfEscapement = 0;
-			lf.lfItalic = 1;
-			lf.lfUnderline = 0;
-			lf.lfStrikeOut = 0; 
-			
-			HFONT hFont = CreateFontIndirect(&lf);
-			SelectObject(ps.hdc, hFont);
-			drawCircleText(ps.hdc, hWnd, rect.right / 2, rect.bottom / 2);
-			
-			DeleteObject(hFont);
+			if(currentMode == MODE_TABLE1)
+			{
+				drawTextTable(ps.hdc, hWnd, rows, cols);
+			}
+			else if (currentMode == MODE_CIRCLE)
+			{
+				drawCircleText(ps.hdc, hWnd, rect.right / 2, rect.bottom / 2);	
+			}
+			else if(currentMode == MODE_TABLE2)
+			{
+				drawSecondTextTable(GetDC(hWnd), hWnd);
+			}
 			EndPaint(hWnd, &ps);
 			break;
 		}
@@ -78,6 +267,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			InvalidateRect(hWnd, NULL, TRUE);
 			break;
 		}
+		case WM_COMMAND:
+		{
+			onCommand(hWnd, wParam);
+			break;
+		}
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
 			break;
@@ -85,11 +279,32 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
+HFONT getCircleFont()
+{
+	LOGFONT lf;
+	lf.lfCharSet = DEFAULT_CHARSET;
+	lf.lfPitchAndFamily = FIXED_PITCH;
+	strcpy(lf.lfFaceName, "Courier New");
+	lf.lfHeight = 20;
+	lf.lfWidth = 10;
+	lf.lfWeight = 400;
+	lf.lfEscapement = 0;
+	lf.lfItalic = 1;
+	lf.lfUnderline = 0;
+	lf.lfStrikeOut = 0; 
+				
+	HFONT hFont = CreateFontIndirect(&lf);
+	return hFont;
+}
+
 void drawCircleText(HDC hdc, HWND hWnd, int x, int y)
 {	
+
+	HFONT hFont = getCircleFont();
+	SelectObject(hdc, hFont);
+	
 	float angle = 3.1415 / 4;
 	
-	SetGraphicsMode(hdc, GM_ADVANCED);
 	
 	XFORM xform;
 	
@@ -99,6 +314,7 @@ void drawCircleText(HDC hdc, HWND hWnd, int x, int y)
 	xform.eM22 = cos (angle);
 	xform.eDx = 0;
 	xform.eDy = 0;
+	SetGraphicsMode(hdc, GM_ADVANCED);
 	SetWorldTransform(hdc, &xform);
 	
 	RECT rect;
@@ -121,6 +337,8 @@ void drawCircleText(HDC hdc, HWND hWnd, int x, int y)
 		SetWorldTransform(hdc, &xform);
 		DrawText(hdc, strOutput + i, 1, &rect, DT_CENTER);
 	}
+	
+	DeleteObject(hFont);
 }
 
 //m - rows, n - cols
@@ -160,6 +378,14 @@ void drawTextTable(HDC hdc, HWND hWnd, int rows, int cols)
 	DeleteObject(hFont); 
 }
 
+void WriteFile(const char* fileName)
+{
+	FILE *f;
+	f = fopen(fileName, "w");
+	fwrite(strOutput, 1, strlen(strOutput), f);
+	fclose(f);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
 {
 WNDCLASSEX wc; 
@@ -191,6 +417,8 @@ WNDCLASSEX wc;
 		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
 		return 0;
 	}
+	
+	addMenu(hWnd);
 	
 	strOutput = (char*)malloc(2);
 	strcpy(strOutput, " ");
